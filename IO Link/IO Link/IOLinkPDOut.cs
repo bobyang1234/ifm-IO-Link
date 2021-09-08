9dcafbe45d7df2f3b5c3a082946aa440d25bec4e
bobyang1234 <qyang2462@gmail.com>
2021-09-08 22:01:24 +1000

    Cleaned up the code, method and classes

---
+++ IO Link/IO Link/IOLinkPDOut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO_Link
+{
+    public class IOLinkPDOut
+    {
+        public string code { get; set; }
+        public int cid { get; set; }
+        public string adr { get; set; }
+        public IOLinkPDOutData data { get; set; }
+    }
+}
---
+++ IO Link/IO Link/ifmAPIRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IO_Link
+{
+    public class ifmAPIRequest
+    {
+
+        public async Task<IOLinkResponse> GetRequest(string url)
+        {
+            using (HttpResponseMessage message = await ifmApiClient.client.GetAsync(url))
+            {
+                if(message.IsSuccessStatusCode)
+                {
+                    IOLinkResponse data = await message.Content.ReadAsAsync<IOLinkResponse>();
+                    return data;
+                }
+                else
+                {
+                    throw new Exception(message.ReasonPhrase);
+                }
+            }
+        }
+
+        public async Task<IOLinkResponse> PostRequest(string url, string json)
+        {
+            using (HttpResponseMessage message = await ifmApiClient.client.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json")))
+            {
+                if(message.IsSuccessStatusCode)
+                {
+                    IOLinkResponse data = await message.Content.ReadAsAsync<IOLinkResponse>();
+                    return data;
+                }
+                else
+                {
+                    throw new Exception(message.ReasonPhrase);
+                }
+            }
+        }
+
+    }
+}
